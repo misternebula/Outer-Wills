147a12d0cda819e414995e37cd028b47b8834dc7
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-05-22 23:29:15 +0100

    Removed debug console writeline

--- Outer Wills/Class1.cs
+++ Outer Wills/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using OWML.ModHelper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,19 @@ namespace Outer_Wills
         void Start()
         {
             ModHelper.Console.WriteLine("Start!");
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = ModHelper.Manifest.ModFolderPath + "afarplay.exe",
+                    Arguments = "\"this is a test\"",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
         }
     }
 }
