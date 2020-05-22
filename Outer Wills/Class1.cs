510dafc8607d10a40f17ee19088bc58cc3bddaf9
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-05-22 23:11:45 +0100

    Add project files.

---
+++ Outer Wills/Class1.cs
@@ -0,0 +1,17 @@
+﻿using OWML.ModHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outer_Wills
+{
+    public class Main : ModBehaviour
+    {
+        void Start()
+        {
+            ModHelper.Console.WriteLine("Start!");
+        }
+    }
+}
